c4658cea053aa6c4712ce87f8835f3e3185791ff
Anwesh43 <anweshthecool0@gmail.com>
2017-05-01 10:18:21 +0530

    Adding SwitchSelectView

---
+++ switchselectlist/src/main/java/com/anwesome/ui/switchselectlist/SwitchSelectView.java
@@ -0,0 +1,19 @@
+package com.anwesome.ui.switchselectlist;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by anweshmishra on 01/05/17.
+ */
+public class SwitchSelectView extends View {
+    private String option;
+    private int time = 0;
+    private Switch mSwitch = new Switch();
+    private AnimationHandler animationHandler;
+    public SwitchSelectView(Context context,String option) {
+        super(context);
+        this.option = option;
+    }
+
+}
